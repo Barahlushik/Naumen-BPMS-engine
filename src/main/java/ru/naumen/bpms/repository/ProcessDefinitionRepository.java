97df4d6b73b6c7fd7935930bfabbcb8c4289b51f
Mark <marik.kriger@yandex.ru>
2026-04-06 10:31:12 +0300

    Добавление метода findAllWithStepsAndTransitions в репозиторий ProcessDefinitionRepository, @RepositoryRestResource аннотации

--- src/main/java/ru/naumen/bpms/repository/ProcessDefinitionRepository.java
+++ src/main/java/ru/naumen/bpms/repository/ProcessDefinitionRepository.java
@@ -2,10 +2,14 @@ package ru.naumen.bpms.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.naumen.bpms.model.ProcessDefinition;
 
+import java.util.List;
 import java.util.Optional;
 
+
+@RepositoryRestResource(path = "process-definitions", collectionResourceRel = "process-definitions")
 public interface ProcessDefinitionRepository extends CrudRepository<ProcessDefinition, Long> {
 
     @Query("""
@@ -26,10 +30,20 @@ public interface ProcessDefinitionRepository extends CrudRepository<ProcessDefin
     Optional<ProcessDefinition> findWithStepsAndTransitionsById(Long id);
 
     @Query("""
-    SELECT DISTINCT pd
-    FROM ProcessDefinition pd
-    JOIN pd.steps s
-    WHERE s.id = :stepId
-""")
+        SELECT DISTINCT pd
+        FROM ProcessDefinition pd
+        JOIN pd.steps s
+        WHERE s.id = :stepId
+    """)
     Optional<ProcessDefinition> findByStepId(Long stepId);
+
+    @Query("""
+        select distinct pd
+        from ProcessDefinition pd
+        left join fetch pd.steps s
+        left join fetch s.outgoingTransitions ot
+        left join fetch ot.toStep ts
+        order by pd.id
+    """)
+    List<ProcessDefinition> findAllWithStepsAndTransitions();
 }
--- src/main/java/ru/naumen/bpms/repository/UserRepository.java
+++ src/main/java/ru/naumen/bpms/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package ru.naumen.bpms.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.naumen.bpms.model.User;
 
 import java.util.Optional;
 
+@RepositoryRestResource(path = "users", collectionResourceRel = "users")
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
